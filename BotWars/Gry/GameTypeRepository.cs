d0897f3af7880b30047373cfdc99497355f9c909
Posiek <posiadalaszymon@gmail.com>
2023-11-18 15:49:37 +0100

    Added GameModel GameRepository and TournamentRegistration

---
+++ BotWars/Gry/GameTypeRepository.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace BotWars.Gry
+{
+    public class GameRepository
+    {
+        public int GetAllAvailableGames() {
+            return 0;
+        }
+
+        public int GetAllGames()
+        {
+            return 0;
+        }
+
+        public int UpdateGame(GameModel gameModel)
+        {
+            return 0;
+        }
+
+    }
+}
