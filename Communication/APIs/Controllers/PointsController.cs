35fcb874683546ddf6e83ea84e670963c7ea56a8
JakubNiewadzi <01169600@pw.edu.pl>
2023-11-30 18:21:18 +0100

    repo done, but waiting for service to be done to continue with controller

---
+++ Communication/APIs/Controllers/PointsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.DataAccess.DataBaseEntities;
+using Shared.DataAccess.Services.Results;
+
+namespace Communication.APIs.Controllers;
+
+[Route("api/[controller]")]
+public class PointsController : Controller
+{
+
+    public async Task<ActionResult<ServiceResponse<long>>> GetPointsForPlayer([FromQuery]long playerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ActionResult<ServiceResponse<List<long>>>> GetLeaderBoards()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ActionResult<ServiceResponse<List<PointHistory>>>> GetHistoryOfPointsForPlayer(
+        [FromQuery] long playerId)
+    {
+        throw new NotImplementedException();
+    }
+}
