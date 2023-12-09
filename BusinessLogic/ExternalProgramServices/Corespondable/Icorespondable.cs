55e3db7d5ea0ccad53f7b4b347262cb0271510c4
Jan Stachurski <jan.stachurski@outlook.com>
2023-12-09 19:03:57 +0100

    docker support finally added!!

---
+++ BusinessLogic/ExternalProgramServices/Corespondable/Icorespondable.cs
@@ -0,0 +1,8 @@
+﻿namespace BusinessLogic.ExternalProgramServices.Corespondable
+{
+    public interface ICorespondable
+    {
+        public Task<string?> Send();
+        public void Interrupt();
+    }
+}
