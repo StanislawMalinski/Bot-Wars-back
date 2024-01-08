72235c102b3ec4754f0d58bf0894018fa6c6122a
JakubNiewadzi <01169600@pw.edu.pl>
2024-01-08 15:43:10 +0100

    user settings implemented

---
+++ Shared/DataAccess/MappersInterfaces/IUserSettingsMapper.cs
@@ -0,0 +1,10 @@
+﻿using Shared.DataAccess.DAO;
+using Shared.DataAccess.DataBaseEntities;
+
+namespace Shared.DataAccess.Mappers;
+
+public interface IUserSettingsMapper
+{
+    public UserSettings DtoToUserSettings(UserSettingsDto dto);
+    public UserSettingsDto UserSettingsToDTO(UserSettings tournament);
+}
