64ef31401958e2619a8323fe505dddeab9d41e66
Jan Stachurski <jan.stachurski@outlook.com>
2024-02-09 13:46:55 +0100

    outdated & redundant files removed

--- BusinessLogic/ExternalProgramServices/Constrain/ConcreteConstrain/IFileData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BusinessLogic.ExternalProgramServices.Constrain.ConcreteConstrain
-{
-	public interface IFileData
-	{
-		public string? Filename { get; set; }
-		public byte[]? Data { get; set; }
-	}
-}
