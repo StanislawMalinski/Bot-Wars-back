c9fca618c653e57c8a0473eb792b484b188885f4
Stanleyowskiki <s.t.malinski@gmail.com>
2024-02-14 21:49:15 +0100

    Files moved to the new repo

--- BusinessLogic/Gameplay/ErrorGameResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using BusinessLogic.Gameplay.Interface;
-
-namespace BusinessLogic.Gameplay
-{
-    public class ErrorGameResult : GameResult
-    {
-        public ErrorGameStatus Status { get; set; }
-    }
-}
--- BusinessLogic/Gameplay/Interface/ICorespondable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BusinessLogic.Gameplay.Interface
-{
-    public interface ICorespondable
-    {
-        public Task<string?> Send(string data);
-        public Task Interrupt();
-        public void Run();
-    }
-}
